4b5f273351365182716d4a786e1e969553f3ead2
Sebastian <sebastiansloma@gmail.com>
2023-04-06 10:35:46 +0200

    configured board

---
+++ components/Square.tsx
@@ -0,0 +1,7 @@
+
+
+function Square(){
+    return <button>This is a square</button>
+}
+
+export default Square
--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Board ()  {
+  const [squares, setSquares] =useState(Array(9).fill(null))
+  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
+    Math.round(Math.random() *1) === 1 ? "X" : "O"
+  )
+  const [winner, setWinner] = useState(null)
+
   return (
-    <div>Board</div>
+    <div>
+      <p>Hey {currentPlayer}, it's your turn</p>
+    </div>
   )
 }
 
