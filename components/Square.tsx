842a16f446d4acd3299d06a91ba5f632f62c82eb
Sebastian <sebastiansloma@gmail.com>
2023-04-07 18:13:18 +0200

    finish

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,3 +1,5 @@
+type Player = 'X' | 'O' | 'BOTH' | null;
+
 function Square({
 	value,
 	onClick,
@@ -8,9 +10,15 @@ function Square({
 	onClick: () => void;
 }) {
 	if (!value) {
-		return <button className="square" onClick={onClick} disabled={Boolean(winner)} />;
+		return (
+			<button className='square' onClick={onClick} disabled={Boolean(winner)} />
+		);
 	}
-	return <button className={`square square_${value.toLocaleLowerCase()}`} disabled>{value}</button>;
+	return (
+		<button className={`square square_${value.toLocaleLowerCase()}`} disabled>
+			{value}
+		</button>
+	);
 }
 
 export default Square;
