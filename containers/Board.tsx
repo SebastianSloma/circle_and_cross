d722acea97218b2050ce9af0d149b09191f08437
Sebastian <sebastiansloma@gmail.com>
2023-04-07 09:16:45 +0200

    .

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,17 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function Board ()  {
-  const [squares, setSquares] =useState(Array(9).fill(null))
-  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
-    Math.round(Math.random() *1) === 1 ? "X" : "O"
-  )
-  const [winner, setWinner] = useState(null)
+function Board() {
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>(
+		Math.round(Math.random() * 1) === 1 ? 'X' : 'O'
+	);
+	const [winner, setWinner] = useState(null);
 
-  return (
-    <div>
-      <p>Hey {currentPlayer}, it's your turn</p>
-    </div>
-  )
+  function setSquareValue(index){
+    const newData=squares.map((val,i) =>{
+      if(i===index){
+        return currentPlayer
+      }
+      return val
+    })
+  }
+
+	return (
+		<div>
+			<p>Hey {currentPlayer}, its your turn</p>
+
+			{Array(9)
+				.fill(null)
+				.map((_, i) => {
+					return (
+						<Square
+							winner={winner}
+							key={i}
+							onClick={() => setSquareValue(i)}
+							value={squares[i]}
+						/>
+					);
+				})}
+		</div>
+	);
 }
 
-export default Board
+export default Board;
