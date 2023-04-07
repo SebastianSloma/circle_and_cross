ecfeaa29563d0be8275dbf0c87809f14f078370d
Sebastian <sebastiansloma@gmail.com>
2023-04-07 16:59:25 +0200

    added squere logic and style css

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,7 +1,16 @@
-
-
-function Square(){
-    return <button>This is a square</button>
+function Square({
+	value,
+	onClick,
+	winner,
+}: {
+	winner: Player;
+	value: Player;
+	onClick: () => void;
+}) {
+	if (!value) {
+		return <button className="square" onClick={onClick} disabled={Boolean(winner)} />;
+	}
+	return <button className={`square square_${value.toLocaleLowerCase()}`} disabled>{value}</button>;
 }
 
-export default Square
+export default Square;
--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Square from '@/components/Square';
 
 function Board() {
 	const [squares, setSquares] = useState(Array(9).fill(null));
@@ -7,31 +8,39 @@ function Board() {
 	);
 	const [winner, setWinner] = useState(null);
 
-  function setSquareValue(index){
-    const newData=squares.map((val,i) =>{
-      if(i===index){
-        return currentPlayer
-      }
-      return val
-    })
-  }
+	function reset(){
+		setSquares(Array(9).fill(null))
+		setWinner(null)
+		setCurrentPlayer(Math.round(Math.random()*1) === 1? "X" : "O")
+	}
+
+	function setSquareValue(index) {
+		const newData = squares.map((val, i) => {
+			if (i === index) {
+				return currentPlayer;
+			}
+			return val;
+		});
+	}
 
 	return (
 		<div>
 			<p>Hey {currentPlayer}, its your turn</p>
-
-			{Array(9)
-				.fill(null)
-				.map((_, i) => {
-					return (
-						<Square
-							winner={winner}
-							key={i}
-							onClick={() => setSquareValue(i)}
-							value={squares[i]}
-						/>
-					);
-				})}
+			<div className='grid'>
+				{Array(9)
+					.fill(null)
+					.map((_, i) => {
+						return (
+							<Square
+								winner={winner}
+								key={i}
+								onClick={() => setSquareValue(i)}
+								value={squares[i]}
+							/>
+						);
+					})}
+			</div>
+			<button className='reset' onClick={reset}>RESET</button>
 		</div>
 	);
 }
